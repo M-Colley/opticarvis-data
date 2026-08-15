17adf26c6dce38ac20c62d56656322d96d211c00
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2026-08-15 11:58:31 +0100

    Update Auswertung_OptiCarVis_Visualizations_CHI25.R

--- Auswertung_OptiCarVis_Visualizations_CHI25.R
+++ Auswertung_OptiCarVis_Visualizations_CHI25.R
@@ -1,5 +1,12 @@
-library("rstudioapi")
-setwd(dirname(getActiveDocumentContext()$path))
+if (requireNamespace("rstudioapi", quietly = TRUE) && rstudioapi::isAvailable()) {
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+} else {
+  args <- commandArgs(trailingOnly = FALSE)
+  file_arg <- grep("^--file=", args, value = TRUE)
+  if (length(file_arg) > 0) {
+    setwd(dirname(normalizePath(sub("^--file=", "", file_arg))))
+  }
+}
 
 library(data.table)
 
